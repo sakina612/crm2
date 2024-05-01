ec6b40a4d8a4ff1dc47e3aacd55ec643bb3a6f10
Gagan <that.me.agastya.rao@gmail.com>
2024-05-01 18:57:15 +0530

    .

--- app/src/main/java/com/example/crms1/admin.java
+++ app/src/main/java/com/example/crms1/admin.java
@@ -9,7 +9,7 @@ import android.view.View;
 
 public class admin extends AppCompatActivity implements View.OnClickListener{
 
-    private CardView card1, card2, card3, card4, card5, card6;
+    private CardView card1, card2,  card4, card6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +25,9 @@ public class admin extends AppCompatActivity implements View.OnClickListener{
 
         card1.setOnClickListener(this);
         card2.setOnClickListener(this);
-        card3.setOnClickListener(this);
+    //    card3.setOnClickListener(this);
         card4.setOnClickListener(this);
-        card5.setOnClickListener(this);
+    //    card5.setOnClickListener(this);
         card6.setOnClickListener(this);
     }
 
@@ -43,8 +43,6 @@ public class admin extends AppCompatActivity implements View.OnClickListener{
             i = new Intent(this, Viewstudent_companyActivity.class);
         } else if (viewId == R.id.viewcompaniesCard) {
             i = new Intent(this, Known_StudentActivity.class);
-        } else if (viewId == R.id.viewjobCard) {
-            i = new Intent(this, ViewjobActivity.class);
         } else if (viewId == R.id.logoutCard) {
             i = new Intent(this, MainActivity.class);
         } else {
